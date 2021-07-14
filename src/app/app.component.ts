7a99fec6d75e18f048eb0a840aecd47e75577b66
Piotr Szok <piotr.szok@gmail.com>
2021-07-14 10:47:28 +0200

    Action button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-hello';
+  link = 'link';
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
